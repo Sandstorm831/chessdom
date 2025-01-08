2fb45d585f514841f354a996a8749062b79fde57
rohan <gargrohan138@gmail.com>
2025-01-08 22:18:35 +0530

    adding engine starter ender functions

--- app/dashboard/chessboard/stockfish.ts
+++ app/dashboard/chessboard/stockfish.ts
@@ -1,15 +1,44 @@
 const stockfish_executable_path =
   "./../../../stockfish/stockfish/stockfish-ubuntu-x86-64-avx2";
-import { spawn } from "node:child_process";
+import { ChildProcessWithoutNullStreams, spawn } from "node:child_process";
 const controller = new AbortController();
 const { signal } = controller;
 
-async function getBestMove() {
+let stockFish: ChildProcessWithoutNullStreams; 
+
+export async function startTheEngine(){
+    let isReady = false;
+    function listener(data: any){
+        isReady = true;
+    }
+    stockFish = spawn(stockfish_executable_path, [], {
+        stdio: ["pipe", "pipe", "pipe"],
+        signal: signal,
+      });
+    stockFish.stdout.on('data', (data) => listener(data));
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    if(isReady) {
+        stockFish.stdout.removeAllListeners('data');
+        return true;
+    }
+    else {
+        console.log("Stockfish engine is taking longer than expected to start ...");
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        stockFish.stdout.removeAllListeners('data');
+        if(!isReady) throw new Error("Failed to start the engine, Engine took too long to start");
+        else return true;
+    }
+}
+
+export async function shutEngineDown(){
+    stockFish.stdin.write('quit\n');
+    console.log('shutting down stockfish engine');
+    stockFish.stdin.end();
+}
+
+export async function getBestMove() {
   const outputArray: string[] = [];
-  const stockFish = spawn(stockfish_executable_path, [], {
-    stdio: ["pipe", "pipe", "pipe"],
-    signal: signal,
-  });
+
   console.log(stockFish.pid);
   stockFish.stdout.on("data", (data) => {
     const c = data.toString().split('\n').filter((str:string) => str);
