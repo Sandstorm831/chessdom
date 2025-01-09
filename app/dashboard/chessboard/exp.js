e3c935f90cd895ca8d9b67180c899941363371aa
rohan <gargrohan138@gmail.com>
2025-01-09 18:13:49 +0530

    made barebone structure for stockfish

---
+++ app/dashboard/chessboard/exp.js
@@ -0,0 +1,37 @@
+import { spawn } from 'child_process';
+import fs from 'fs';
+
+async function ReadFile(){
+    const runningPID = await fs.readFile('running.pid', 'utf-8', (err, data) => {
+        if(err){
+          console.log(err);
+          throw new Error('error in reading pid file');
+        }
+        console.log(data.length);
+      })
+    //   return;
+}
+
+// await ReadFile();
+
+//   const terminal = spawn('bash');
+//   terminal.stdout.on('data', (data) => {console.log(`OUTPUT : ${data}`)});
+//   terminal.on('close', (code) => {
+//     console.log("closing terminal with exit code = " + code);
+//   })
+//   await new Promise(resolve => setTimeout(resolve, 2000));
+//   terminal.stdin.write('help\n');
+//   await new Promise(resolve => setTimeout(resolve, 2000));
+//   terminal.stdout.removeAllListeners();
+//   terminal.stdin.write('pwd\n');
+//   await new Promise(resolve => setTimeout(resolve, 2000));
+//   terminal.stdout.on('data', (data) => {console.log(`OUTPUT : ${data}`)});
+//   terminal.stdin.write('ls\n');
+//   await new Promise(resolve => setTimeout(resolve, 10000));
+  terminal.stdin.write('exit\n');
+
+// fs.writeFile('running.pid', '', (err) => {
+//     if(err){
+//         throw new Error(err);
+//     }
+// })
--- app/dashboard/chessboard/stockfish.ts
+++ app/dashboard/chessboard/stockfish.ts
@@ -3,11 +3,44 @@ const stockfish_executable_path =
 import { ChildProcessWithoutNullStreams, spawn } from "node:child_process";
 const controller = new AbortController();
 const { signal } = controller;
+import fs from 'fs/promises';
+let lastProcess: string = 'null';
 
 let stockFish: ChildProcessWithoutNullStreams; 
 
-export async function startTheEngine(){
+async function clearFile(){
+  try{
+    await fs.writeFile('running.pid','', 'utf-8');
+    return;
+
+  } catch(err){
+    throw new Error('Error in clearing pid file')
+  }
+}
+
+async function terminatePIDs(PIDList: string[]){
+  const terminal = spawn('bash');
+  for(let i=0; i<PIDList.length; i++){
+    terminal.stdin.write(`kill ${PIDList[i]}\n`);
+    await new Promise(resolve => setTimeout(resolve, 50));
+  }
+  terminal.stdin.write('exit\n');
+  await clearFile();
+  return;
+}
+
+export async function startTheEngine(elo: string = '1320'){
     let isReady = false;
+    try{
+      const data = await fs.readFile('running.pid', 'utf-8')
+      if(data.length !== 0){
+        await terminatePIDs(data.split('\n'));
+      }
+    } catch (err){
+      console.log(err);
+      throw new Error('error in reading pid file');
+    }
+
     function listener(data: any){
         isReady = true;
     }
@@ -15,41 +48,90 @@ export async function startTheEngine(){
         stdio: ["pipe", "pipe", "pipe"],
         signal: signal,
       });
+    const PID = stockFish.pid;
+    if(PID === undefined) throw new Error('error in getting PID of spawned process');
+    fs.writeFile('running.pid', PID.toString(), 'utf-8');
     stockFish.stdout.on('data', (data) => listener(data));
+    stockFish.on("close", (code) => {
+      console.log(`Process is closed with exit code ${code}`);
+    });
+    lastProcess = 'startTheEngine';
     await new Promise(resolve => setTimeout(resolve, 1000));
     if(isReady) {
-        stockFish.stdout.removeAllListeners('data');
+        stockFish.stdin.write('setoption name Threads value 2\n');                  // setting option
+        stockFish.stdin.write('setoption name Hash value 64\n');                    // setting option
+        stockFish.stdin.write('setoption name MultiPV value 1');                    // setting option
+        stockFish.stdin.write('setoption name UCI_LimitStrength value true\n');     // setting option
+        stockFish.stdin.write(`setoption name UCI_Elo value ${elo}\n`);             // setting option
         return true;
     }
     else {
         console.log("Stockfish engine is taking longer than expected to start ...");
         await new Promise(resolve => setTimeout(resolve, 5000));
-        stockFish.stdout.removeAllListeners('data');
         if(!isReady) throw new Error("Failed to start the engine, Engine took too long to start");
-        else return true;
+        else {
+          stockFish.stdin.write('setoption name Threads value 2\n');                  // setting option
+          stockFish.stdin.write('setoption name Hash value 64\n');                    // setting option
+          stockFish.stdin.write('setoption name MultiPV value 1');                    // setting option
+          stockFish.stdin.write('setoption name UCI_LimitStrength value true\n');     // setting option
+          stockFish.stdin.write(`setoption name UCI_Elo value ${elo}\n`);             // setting option
+          return true;
+        }
     }
 }
 
 export async function shutEngineDown(){
     stockFish.stdin.write('quit\n');
+    lastProcess = 'null';
+    try{
+      const data = await fs.readFile('running.pid', 'utf-8')
+      if(data.length !== 0){
+        await terminatePIDs(data.split('\n'));
+      }
+    } catch (err){
+      console.log(err);
+      throw new Error('error in reading pid file for terminating engine');
+    }
     console.log('shutting down stockfish engine');
     stockFish.stdin.end();
 }
 
-export async function getBestMove() {
+export async function getBestMove(fen: string) {
   const outputArray: string[] = [];
+  if(lastProcess !== 'getBestMove'){
+    // Removing listeners
+    stockFish.stdout.removeAllListeners();
+    stockFish.stderr.removeAllListeners();
+    // Adding listeners
+    stockFish.stdout.on("data", (data) => {
+      const c = data.toString().split('\n').filter((str:string) => str);
+      for(let i=0; i<c.length; i++) outputArray.push(c[i]);
+    });
+    stockFish.stderr.on("error", (error) => {
+      console.log(`Error : ${error.toString()}`);
+    });
+    stockFish.on("close", (code) => {
+      console.log(`Process is closed with exit code ${code}`);
+    });
+    // marking the current function
+    lastProcess = 'getBestMOve';
+  }
 
-  console.log(stockFish.pid);
-  stockFish.stdout.on("data", (data) => {
-    const c = data.toString().split('\n').filter((str:string) => str);
-    for(let i=0; i<c.length; i++) outputArray.push(c[i]);
-  });
-  stockFish.stderr.on("error", (error) => {
-    console.log(`Error : ${error.toString()}`);
-  });
-  stockFish.on("close", (code) => {
-    console.log(`Process is closed with exit code ${code}`);
-  });
+  // Checking the health of Engine
+  stockFish.stdin.write('isready\n');
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  if(outputArray[1] !== 'readyok'){
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    if(outputArray[1] !== 'readyok'){
+      throw new Error('something is wrong with the engine');
+    }
+  }
+
+  // Setting the FEN
+  stockFish.stdin.write(`position fen ${fen}`);
+  await new Promise(resolve => setTimeout(resolve, 200));
+
+  // Finding the best move
   stockFish.stdin.write("go depth 15\n");
   await new Promise((resolve => setTimeout(resolve, 1000)));
   stockFish.stdin.write("quit");
@@ -57,8 +139,8 @@ export async function getBestMove() {
   return outputArray[outputArray.length - 1].split(" ")[1];
 }
 
-async function main(){
-    const bestMove = await getBestMove();
-    console.log(bestMove)
-}
-main();
+
+// error
+// end
+// data
+// close
