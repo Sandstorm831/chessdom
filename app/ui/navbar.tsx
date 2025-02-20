f4a9577ed4aeefcdcffe826e232300026465c415
Sandstorm831 <gargrohan138@gmail.com>
2025-02-20 18:47:04 +0530

    making navigation intuitive and adding aboutme and attributions page links

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -9,6 +9,8 @@ import Link from "next/link";
 import { GiMountedKnight } from "react-icons/gi";
 import { MdDashboard } from "react-icons/md";
 import { FaHome } from "react-icons/fa";
+import { GoCommandPalette } from "react-icons/go";
+import { TbHeartHandshake } from "react-icons/tb";
 
 export default async function Navbar() {
   const session = await auth();
@@ -19,44 +21,77 @@ export default async function Navbar() {
       </div>
       <div>
         {session?.user ? (
-          <Popover>
-            <PopoverTrigger asChild>
-              <GiMountedKnight className="cursor-pointer text-5xl" />
-            </PopoverTrigger>
-            <PopoverContent className="w-max flex flex-col p-2">
-              {session.user.email ? (
-                <div className="text-3xl text-[#323014] my-2 mx-5">{`Hello ${session.user.email.split("@")[0]}`}</div>
-              ) : (
-                "Hello Knight"
-              )}
-              {session ? (
-                <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
-                  <div className="flex flex-col justify-center ms-5 mr-2">
-                    <FaHome />
-                  </div>
-                  <div className="flex flex-col justify-center">
-                    <Link href={"/"}>Home</Link>
-                  </div>
-                </div>
-              ) : null}
-              {session ? (
-                <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
-                  <div className="flex flex-col justify-center ms-5 mr-2">
-                    <MdDashboard />
-                  </div>
-                  <div className="flex flex-col justify-center">
-                    <Link href={"/dashboard"}>Dashboard</Link>
-                  </div>
-                </div>
-              ) : null}
-              <div className="w-full px-5 mt-2">
-                <SignOut
-                  variant="default"
-                  className="text-xl bg-[#323014] text-[#FFFEFC] w-full"
-                />
+          <div className="flex justify-evenly">
+            <div className="text-2xl mx-5">
+              <div className="flex flex-col justify-center h-full">
+                <Link
+                  href="/dashboard"
+                  className="flex justify-center cursor-pointer hover:underline transition duration-150"
+                >
+                  <MdDashboard className="h-full" />
+                  dashboard
+                </Link>
               </div>
-            </PopoverContent>
-          </Popover>
+            </div>
+            <Popover>
+              <PopoverTrigger asChild>
+                <GiMountedKnight className="cursor-pointer text-5xl" />
+              </PopoverTrigger>
+              <PopoverContent className="w-max flex flex-col p-2">
+                {session.user.email ? (
+                  <div className="text-3xl text-[#323014] my-2 mx-5">{`Hello ${session.user.email.split("@")[0]}`}</div>
+                ) : (
+                  "Hello Knight"
+                )}
+                {session ? (
+                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
+                    <div className="flex flex-col justify-center ms-5 mr-2">
+                      <FaHome />
+                    </div>
+                    <div className="flex flex-col justify-center">
+                      <Link href={"/"}>Home</Link>
+                    </div>
+                  </div>
+                ) : null}
+                {session ? (
+                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
+                    <div className="flex flex-col justify-center ms-5 mr-2">
+                      <MdDashboard />
+                    </div>
+                    <div className="flex flex-col justify-center">
+                      <Link href={"/dashboard"}>Dashboard</Link>
+                    </div>
+                  </div>
+                ) : null}
+                {session ? (
+                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
+                    <div className="flex flex-col justify-center ms-5 mr-2">
+                      <GoCommandPalette />
+                    </div>
+                    <div className="flex flex-col justify-center">
+                      <Link href={"/aboutme"}>About Me</Link>
+                    </div>
+                  </div>
+                ) : null}
+                {session ? (
+                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
+                    <div className="flex flex-col justify-center ms-5 mr-2">
+                      <TbHeartHandshake />
+                    </div>
+                    <div className="flex flex-col justify-center">
+                      <Link href={"/attribution"}>Attributions</Link>
+                    </div>
+                  </div>
+                ) : null}
+                <div className="w-full px-5 mt-2">
+                  <SignOut
+                    variant="default"
+                    className="text-xl bg-[#323014] text-[#FFFEFC] w-full"
+                  />
+                </div>
+              </PopoverContent>
+            </Popover>
+          </div>
         ) : (
           <SignIn variant="link" className="text-3xl text-[#fffefc]" />
         )}
