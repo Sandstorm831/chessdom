1f6b29d0eeff519cff184d724588f3d6e4ceec75
rohan <gargrohan138@gmail.com>
2025-12-06 15:53:21 +0530

    fixed navbar display bugs

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -11,12 +11,13 @@ import { MdDashboard } from "react-icons/md";
 import { FaHome, FaGithub } from "react-icons/fa";
 import { GoCommandPalette } from "react-icons/go";
 import { TbHeartHandshake } from "react-icons/tb";
+import { PopoverClose } from "@radix-ui/react-popover";
 
 export default async function Navbar() {
   const session = await auth();
   return (
     <div className="w-full flex justify-between pt-3 px-3 text-[#fffefc] bg-[#323014]">
-      <div className="text-xl md:text-3xl">
+      <div className="text-xl md:text-3xl flex flex-col justify-center">
         <Link href={"/"}> Chessdom </Link>
       </div>
       <div>
@@ -39,61 +40,86 @@ export default async function Navbar() {
               </PopoverTrigger>
               <PopoverContent className="w-max flex flex-col p-2">
                 {session.user.email ? (
-                  <div className="text-3xl text-[#323014] my-2 mx-5">{`Hello ${session.user.email.split("@")[0]}`}</div>
+                  <div className="text-3xl text-[#323014] my-2 mx-5">{`Hello ${
+                    session.user.email.split("@")[0]
+                  }`}</div>
                 ) : (
                   "Hello Knight"
                 )}
                 {session ? (
-                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
-                    <div className="flex flex-col justify-center ms-5 mr-2">
-                      <FaHome />
-                    </div>
-                    <div className="flex flex-col justify-center">
-                      <Link href={"/"}>Home</Link>
-                    </div>
-                  </div>
+                  <PopoverClose asChild>
+                    <Link
+                      href={"/"}
+                      className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex"
+                    >
+                      <div className="flex flex-col justify-center ms-5 mr-2">
+                        <FaHome />
+                      </div>
+                      <div className="flex flex-col justify-center">Home</div>
+                    </Link>
+                  </PopoverClose>
                 ) : null}
                 {session ? (
-                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
-                    <div className="flex flex-col justify-center ms-5 mr-2">
-                      <MdDashboard />
-                    </div>
-                    <div className="flex flex-col justify-center">
-                      <Link href={"/dashboard"}>Dashboard</Link>
-                    </div>
-                  </div>
+                  <PopoverClose asChild>
+                    <Link
+                      href={"/dashboard"}
+                      className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex"
+                    >
+                      <div className="flex flex-col justify-center ms-5 mr-2">
+                        <MdDashboard />
+                      </div>
+                      <div className="flex flex-col justify-center">
+                        Dashboard
+                      </div>
+                    </Link>
+                  </PopoverClose>
                 ) : null}
                 {session ? (
-                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
-                    <div className="flex flex-col justify-center ms-5 mr-2">
-                      <GoCommandPalette />
-                    </div>
-                    <div className="flex flex-col justify-center">
-                      <Link href={"/aboutme"}>About Me</Link>
-                    </div>
-                  </div>
+                  <PopoverClose asChild>
+                    <Link
+                      href={"/aboutme"}
+                      className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex"
+                    >
+                      <div className="flex flex-col justify-center ms-5 mr-2">
+                        <GoCommandPalette />
+                      </div>
+                      <div className="flex flex-col justify-center">
+                        About me
+                      </div>
+                    </Link>
+                  </PopoverClose>
                 ) : null}
                 {session ? (
-                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
-                    <div className="flex flex-col justify-center ms-5 mr-2">
-                      <TbHeartHandshake />
-                    </div>
-                    <div className="flex flex-col justify-center">
-                      <Link href={"/attribution"}>Attributions</Link>
-                    </div>
-                  </div>
+                  <PopoverClose asChild>
+                    <Link
+                      href={"/attribution"}
+                      className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex"
+                    >
+                      <div className="flex flex-col justify-center ms-5 mr-2">
+                        <TbHeartHandshake />
+                      </div>
+                      <div className="flex flex-col justify-center">
+                        Attributions
+                      </div>
+                    </Link>
+                  </PopoverClose>
                 ) : null}
                 {session ? (
-                  <div className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex">
-                    <div className="flex flex-col justify-center ms-5 mr-2">
-                      <FaGithub />
-                    </div>
-                    <div className="flex flex-col justify-center">
-                      <Link href={"https://github.com/sandstorm831/chessdom"}>
+                  <PopoverClose asChild>
+                    <a
+                      href="https://github.com/sandstorm831/chessdom"
+                      className="bg-[#fffefec] text-[#323014] hover:bg-[#323014] hover:bg-opacity-10 text-lg cursor-pointer border-[#fffefc] rounded-md flex"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <div className="flex flex-col justify-center ms-5 mr-2">
+                        <FaGithub />
+                      </div>
+                      <div className="flex flex-col justify-center">
                         GitHub Repo
-                      </Link>
-                    </div>
-                  </div>
+                      </div>
+                    </a>
+                  </PopoverClose>
                 ) : null}
                 <div className="w-full px-5 mt-2">
                   <SignOut
